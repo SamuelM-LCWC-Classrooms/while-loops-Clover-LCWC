59ecf5df35e01c1f37f5df49c6dc1553cb78a822
Clover <144791981+Clover-LCWC@users.noreply.github.com>
2024-09-25 10:40:38 +0000

    stream october song by toastywav

--- task.py
+++ task.py
@@ -1,11 +1,25 @@
+import time
+
 def task_1():
     password = "Password123"
+    guess = input("Guess the password: ")
+    while guess != password:
+        print("Password incorrect, try again!")
+        guess = input("Guess the password: ")
+    print("Password cracked!")
 
-    # Enter your code here
 
-def task_2(): # Times table
+def task_2():
+    times_table = int(input("Enter the times table number: "))
+    terms = int(input("Enter the number of terms to calculate: "))
+    count = 1
+    while count <= terms:
+        print(times_table * count)
+        count += 1
 
-    # Enter your code here
-
-def task_3(): # Count mississippis
-    
+def task_3():
+    count = 1
+    while count <= 5:
+        print(f"{count} Mississippi")
+        time.sleep(1)
+        count += 1
